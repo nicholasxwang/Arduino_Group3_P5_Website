e1d9199bff21f31cca1f03a336d0b7b0c3380102
Nicholas Wang <80929436+nicholasxwang@users.noreply.github.com>
2022-01-07 20:50:11 +0000

    added htmls

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 app = Flask('app')
 
-@app.route('/')
-def hello_world():
-  return 'Hello, World!'
 
+@app.route('/login')
+def login():
+  return render_template("login.html")
+@app.route('/signup')
+def signup():
+  return render_template("signup.html")
+
+@app.route("/dashboard")
+def dashboard():
+  return render_template
+  ("dashboard.html")
+@app.route("/")
+def homescreen():
+  username = request.cookies.get('u')
+  password = request.cookies.get('p')
+  if not username and not password:
+    return redirect("/login")
+  return redirect("/dashboard")
 app.run(host='0.0.0.0', port=8080)
